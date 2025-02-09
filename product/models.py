88c09982b658b5fa28e078e93baf763abfbef088
Messorah <garabrielra@gmail.com>
2025-02-09 15:59:15 -0300

    criando o models de Product e colocando na admin

--- product/models.py
+++ product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    stock = models.PositiveIntegerField()
+    barcode = models.PositiveIntegerField()

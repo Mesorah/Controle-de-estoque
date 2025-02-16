8c544886d635abbf18c094e1fc531fc79f200bad
Messorah <garabrielra@gmail.com>
2025-02-16 12:25:50 -0300

    Alterando o login/logout para funcionar corretamente

--- authors/views.py
+++ authors/views.py
@@ -5,10 +5,14 @@ from django.urls import reverse_lazy
 class AuthorLoginView(LoginView):
     redirect_authenticated_user = True
     template_name = 'authors/login.html'
+    success_url = reverse_lazy('product:home')
 
-    def get_success_url(self):
-        return reverse_lazy('product:home')
+    def get_redirect_url(self):
+        return self.success_url
 
 
 class AuthorLogoutView(LogoutView):
-    success_url = reverse_lazy('product:home')
+    success_url = reverse_lazy('authors:login')
+
+    def get_redirect_url(self):
+        return self.success_url

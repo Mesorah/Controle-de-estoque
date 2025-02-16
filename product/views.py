cdf45c720aa1d56c173acc4934930992053779d7
Messorah <garabrielra@gmail.com>
2025-02-16 19:18:43 -0300

    colocando o select_related para melhorar no banco

--- product/views.py
+++ product/views.py
@@ -7,10 +7,17 @@ from product import models
 class HomeListView(ListView):
     allow_empty = True
     model = models.Product
-    paginate_by = 1
+    paginate_by = 10
     context_object_name = 'products'
     ordering = '-id'
     template_name = 'product/home.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        qs = qs.select_related('category')
+
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

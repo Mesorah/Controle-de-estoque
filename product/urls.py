f809862a5045640f905f8febea8fe6ab4ae3b1e9
Messorah <garabrielra@gmail.com>
2025-03-01 20:13:40 -0300

    criando uma função para facilitar a repetição do staff_member_required

--- product/urls.py
+++ product/urls.py
@@ -5,58 +5,54 @@ from product import views
 
 app_name = 'product'
 
+
+def staff_member(view):
+    return staff_member_required(
+        view,
+        login_url=reverse_lazy('authors:login')
+    )
+
+
 urlpatterns = [
     path(
         '',
-        staff_member_required(
-            views.HomeListView.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.HomeListView.as_view()),
         name='home'
     ),
 
     path(
         'search/',
-        staff_member_required(
-            views.ProductSearch.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.ProductSearch.as_view()),
         name='search'
     ),
 
     path(
         'create/',
-        staff_member_required(
-            views.CreateProductSession.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.CreateProductSession.as_view()),
         name='create'
     ),
 
     path(
         'update/<int:pk>/',
-        staff_member_required(
-            views.UpdateProduct.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.UpdateProduct.as_view()),
         name='update'
     ),
 
     path(
+        'update_session/<int:id>/',
+        staff_member(views.UpdateProductSession.as_view()),
+        name='update_session'
+    ),
+
+    path(
         'delete/<str:id>/',
-        staff_member_required(
-            views.DeleteProductSession.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.DeleteProductSession.as_view()),
         name='delete'
     ),
 
     path(
         'dashboard/',
-        staff_member_required(
-            views.Dashboard.as_view(),
-            login_url=reverse_lazy('authors:login')
-        ),
+        staff_member(views.Dashboard.as_view()),
         name='dashboard'
     ),
 ]

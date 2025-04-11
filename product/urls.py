afa1f793eafad1c17aae656a48997b1ddb73887e
Messorah <garabrielra@gmail.com>
2025-04-11 11:05:37 -0300

    usando ModelViewSet com Rounters

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,16 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path, reverse_lazy
+from rest_framework.routers import SimpleRouter
 
 from product import views
+
+product_api_router = SimpleRouter()
+
+product_api_router.register(
+    'api',
+    views.ProducAPIViewSet,
+    basename='api'
+)
 
 app_name = 'product'
 
@@ -56,14 +65,16 @@ urlpatterns = [
         name='dashboard'
     ),
 
-    path(
-        'api/',
-        views.ProductListAPIView.as_view(),
-        name='product_list'
-    ),
-    path(
-        'api/<int:pk>/',
-        views.ProductDetailAPIView.as_view(),
-        name='product_detail'
-    ),
+    # path(
+    #     'api/',
+    #     views.ProducAPIViewSet.as_view(),
+    #     name='product_list'
+    # ),
+    # path(
+    #     'api/<int:pk>/',
+    #     views.ProducAPIViewSet.as_view(),
+    #     name='product_detail'
+    # ),
 ]
+
+urlpatterns += product_api_router.urls
--- product/views/api.py
+++ product/views/api.py
@@ -3,6 +3,7 @@ from rest_framework.generics import (  # noqa E501
     RetrieveUpdateDestroyAPIView,
 )
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
 from product.serializers import ProductSerializer
@@ -10,6 +11,12 @@ from product.serializers import ProductSerializer
 
 class ProductAPIPagination(PageNumberPagination):
     page_size = 5
+
+
+class ProducAPIViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductAPIPagination
 
 
 class ProductListAPIView(ListCreateAPIView):
